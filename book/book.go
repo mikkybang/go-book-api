c757efa3e17c16c7e98a4eb40b454768e0e7de8a
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-12 12:25:33 +0100

    Update NewBook function to parse body requests with bodyParser and create a new book object with it

--- book/book.go
+++ book/book.go
@@ -30,16 +30,27 @@ func GetBook(c *fiber.Ctx) {
 
 func NewBook(c *fiber.Ctx) {
 	db := database.DBConn
-	var book Book
-	book.Title = "1984"
-	book.Author = "George Orwell"
-	book.Rating = 5
+	book := new(Book)
+	if err := c.BodyParser(book); err !nil {
+		c.Status(503).Send(err)
+		return
+	} 
 	db.Create(&book)
 	c.JSON(book)
 }
 
 func DeleteBooks(c *fiber.Ctx) {
-	c.Send("Delets a Book")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+
+	db.First(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No book found with given ID")
+		return
+	}
+	db.Delete(&book)
+	c.Send("Book successfully deleted")
 }
 
 // func GetBooks(c *fiber.Ctx) {
