f48d377fc4155a88a41a70884d0a0f0e434bc2cd
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-12 11:39:03 +0100

    Add SetupRoutes Function

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber"
+	"github.com/mikkybang/go-book-api/book"
 )
 
 func helloWorld(c *fiber.Ctx) {
 	c.Send("Hello World")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
 }
 
 func main() {
