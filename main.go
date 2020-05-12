4ca74595fc2288ae5b4bbb8a1771a37e68adb591
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-12 11:44:39 +0100

    Add setupRoutes function to main function

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:1d", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
-	app.
+	app.Delete("/api/v1/book/:id", book.DeleteBooks)
 }
 
 func main() {
 	app := fiber.New()
 
+	setupRoutes(app)
+	
 	app.Get("/", helloWorld)
 
 	app.Listen(8000)
